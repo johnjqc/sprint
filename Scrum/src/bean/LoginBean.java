4538c44ab58bd051cc1622f8bfba1f552326d2ae
johnqc <sir.jota@yahoo.com>
2014-09-14 13:59:29 -0500

    funcionamioento richfaces con apache 7 ok

---
+++ Scrum/src/bean/LoginBean.java
@@ -0,0 +1,74 @@
+package bean;
+
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import utils.Util;
+
+
+ 
+@ManagedBean(name = "loginBean")
+@SessionScoped
+public class LoginBean implements Serializable {
+ 
+    private static final long serialVersionUID = 1L;
+    private String password;
+    private String message, uname;
+ 
+    public String getMessage() {
+        return message;
+    }
+ 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
+    public String getUname() {
+        return uname;
+    }
+ 
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+ 
+    public String loginProject() {
+        boolean result = true;//UserDAO.login(uname, password);
+        if (result) {
+            // get Http Session and store username
+            HttpSession session = Util.getSession();
+            session.setAttribute("username", uname);
+ 
+            return "home";
+        } else {
+ 
+            FacesContext.getCurrentInstance().addMessage(
+                    null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN,
+                    "Invalid Login!",
+                    "Please Try Again!"));
+ 
+            // invalidate session, and redirect to other pages
+ 
+            //message = "Invalid Login. Please Try Again!";
+            return "login";
+        }
+    }
+ 
+    public String logout() {
+      HttpSession session = Util.getSession();
+      session.invalidate();
+      return "login";
+   }
+}
