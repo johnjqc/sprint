540277a8603bf000e2f54544c45363a5eca049db
jquiroga <jairo.quiroga@alliensoft.com>
2014-09-15 15:02:01 -0500

    estrucura login, y agrego home

--- Scrum/src/bean/LoginBean.java
+++ Scrum/src/bean/LoginBean.java
@@ -1,9 +1,10 @@
 package bean;
 
 import java.io.Serializable;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
@@ -12,7 +13,7 @@ import utils.Util;
 
  
 @ManagedBean(name = "loginBean")
-@SessionScoped
+@ViewScoped
 public class LoginBean implements Serializable {
  
     private static final long serialVersionUID = 1L;
